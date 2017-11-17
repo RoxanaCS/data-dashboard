996bd5d518dcc31baee5360dc40c1ddd342605bb
RoxanaCS <roxale.cares@gmail.com>
2017-11-17 18:16:52 -0300

    verion3_HSE y tecnico

--- js/app.js
+++ js/app.js
@@ -27,26 +27,46 @@ var cargarPagina = function(){
 }
 cargarPagina();
 //grafico de las habilidades tecnicas
-google.charts.load("current", {packages:["corechart"]});
+google.charts.load('current', {'packages':['corechart']});
       google.charts.setOnLoadCallback(drawChart);
+
       function drawChart() {
+
         var data = google.visualization.arrayToDataTable([
-          ['Task', 'Hours per Day'],
+          ['Aprobacion', 'n° alumnas'],
           ['Work',     11],
           ['Eat',      2],
-          ['Commute',  2],
-          ['Watch TV', 2],
-          ['Sleep',    7]
         ]);
 
         var options = {
-          pieHole: 0.4,
+          colors: ['#F9A91A', '#808080']
         };
 
-        var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
         chart.draw(data, options);
       }
+      //grafico de las HSE
+      google.charts.load('current', {'packages':['corechart']});
+            google.charts.setOnLoadCallback(drawChart2);
 
+            function drawChart2() {
+
+              var data = google.visualization.arrayToDataTable([
+                ['Task', 'Hours per Day'],
+                ['Work',     11],
+                ['Eat',      2],
+              ]);
+
+              var options = {
+                colors: ['#F9A91A', '#808080']
+
+              };
+
+              var chart = new google.visualization.PieChart(document.getElementById('piechart2'));
+
+              chart.draw(data, options);
+            }
 
 
 
