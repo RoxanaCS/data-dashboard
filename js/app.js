45e22f2ac50189f2b0092d856393202254e0686b
RoxanaCS <roxale.cares@gmail.com>
2017-11-17 16:14:44 -0300

    tenemos graficos!

--- js/app.js
+++ js/app.js
@@ -1,12 +1,3 @@
-//hacer el menu desplegable para elegir cada sede y generación
-var cargarGeneracion = function(){
-  var elementosTab = document.getElementsByClassName("tab");
-  for(var i = 0; i < elementosTab.length; i++){
-    elementosTab[i].addEventListener("click",mostrarOcultar);
-  }
-}
-cargarPagina();
-
 
 //hacer que el menu cambie cada vez que es seleccionado
 var mostrarOcultar = function(e){
@@ -35,6 +26,29 @@ var cargarPagina = function(){
   }
 }
 cargarPagina();
+//grafico de las habilidades tecnicas
+google.charts.load("current", {packages:["corechart"]});
+      google.charts.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Task', 'Hours per Day'],
+          ['Work',     11],
+          ['Eat',      2],
+          ['Commute',  2],
+          ['Watch TV', 2],
+          ['Sleep',    7]
+        ]);
+
+        var options = {
+          pieHole: 0.4,
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+        chart.draw(data, options);
+      }
+
+
+
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
 //console.log(data);
